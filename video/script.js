fdb5eccb65ec12547980e68d492cb0ab93523d20
faizan <faizanzaid789@gmail.com>
2022-01-09 17:51:46 +0530

    Created a responsive website

---
+++ video/script.js
@@ -0,0 +1,11 @@
+console.log("This is my first responsive website");
+
+//Accessing variables
+const menuItem=document.querySelector('.menuItem');
+const menuItems=document.querySelector('.items');
+
+
+
+menuItem.addEventListener('click', ()=>{
+menuItems.classList.toggle('visibleMenu')
+})
