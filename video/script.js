7f12a58c0cee982436cded8b7e032dc6b90b70ed
faizan <faizanzaid789@gmail.com>
2022-01-09 23:01:26 +0530

    Made some minute changes

--- video/script.js
+++ video/script.js
@@ -1,12 +1,10 @@
 console.log("This is my first responsive website");
 
 //Accessing variables
-const menuItem=document.querySelector('.menuItem');
-const menuItems=document.querySelector('.items');
+const menuItem = document.querySelector(".menuItem");
+const menuItems = document.querySelector(".items");
 
-
-
-menuItem.addEventListener('click', ()=>{
-menuItems.classList.toggle('visibleMenu')
-menuItem.classList.toggle("animateMenu");
-})
+menuItem.addEventListener("click", () => {
+  menuItems.classList.toggle("visibleMenu");
+  menuItem.classList.toggle("animateMenu");
+});
